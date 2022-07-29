23b5bb51ee4dc40d4e47ec60ceeb7a7a1b7579c6
zezeze97 <18428308691@163.com>
2022-07-29 13:35:42 +0800

    move config

--- configs/swin/old/upernet_swin_large_patch4_window7_512x512_pretrain_224x224_22K_160k_ade20k.py
+++ configs/swin/old/upernet_swin_large_patch4_window7_512x512_pretrain_224x224_22K_160k_ade20k.py
@@ -1,4 +1,4 @@
-base_ = [
+_base_ = [
     'upernet_swin_tiny_patch4_window7_512x512_160k_ade20k_'
     'pretrain_224x224_1K.py'
 ]
@@ -11,5 +11,5 @@ model = dict(
         depths=[2, 2, 18, 2],
         num_heads=[6, 12, 24, 48],
         window_size=7),
-        decode_head=dict(in_channels=[192, 384, 768, 1536], num_classes=150),
+    decode_head=dict(in_channels=[192, 384, 768, 1536], num_classes=150),
     auxiliary_head=dict(in_channels=768, num_classes=150))
